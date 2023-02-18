5c54a4e55cf8cb3514ad40415b8390861c35ada2
Mounira2000 <tidjanimounira@gmail.com>
2023-02-18 11:24:56 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
-#include<stdio.h>
-/*this is descriptio
- *
- * n */
+#include <stdio.h>
+/**
+ * main - Programminng is like building a multinational puzzle fonctio
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
