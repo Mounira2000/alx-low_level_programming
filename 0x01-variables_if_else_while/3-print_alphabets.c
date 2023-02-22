7c7486459b2ae727e4d7b7373c6a92b83c128a51
Mounira2000 <tidjanimounira@gmail.com>
2023-02-22 14:58:25 -0800

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,14 +8,21 @@
 
 int main(void)
 {
-	char a[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$";
-
+	char a[26] = "abcdefghijklmnopqrstuvwxyz";
+	char b[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ$";
 	int i = 0;
+	int j = 0;
 
-	for (i = 0; i <= 52; i++)
+	for (i = 0; i <= 26; i++)
 	{
 		putchar(a[i]);
 	}
+
+	for (j = 0; j <= 27; j++)
+	{
+		putchar(a[j]);
+	}
+
 	putchar('\n');
 
 	return (0);
