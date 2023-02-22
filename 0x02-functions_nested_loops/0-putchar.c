571b0b661c0ffd764d67a3b6980660e1385181fc
Mounira2000 <tidjanimounira@gmail.com>
2023-02-22 15:17:27 -0800

    11

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ *
+ */
+
+
+int main(void)
+{
+	char word[8] = "_putchar";
+
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		putchar(word[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
