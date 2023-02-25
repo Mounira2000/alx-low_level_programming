8857d907d81a88b7f3308d0a72d9be40670eeb37
Mounira2000 <tidjanimounira@gmail.com>
2023-02-25 15:37:13 -0800

    v

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * jack_bauer - function that will print the hours
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+
+	int heures;
+	int minutes;
+
+	for (heures = 0; heures <= 23; heures++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			if (minutes < 10 && heures < 10)
+			{
+				printf("0%d:0%d", heures, minutes);
+			}
+			else if (minutes < 10 && heures >= 10)
+			{
+				printf("%d:0%d", heures, minutes);
+			}
+			else if (heures < 10  && minutes >= 10)
+			{
+				printf("0%d:%d", heures, minutes);
+			}
+			else
+			{
+				printf("%d:%d", heures, minutes);
+			}
+		}
+	}
+}
