afe3afe3edfbb95fb9f234faa97aba7db35c2254
Mounira2000 <tidjanimounira@gmail.com>
2023-02-25 08:25:28 -0800

    v

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - function that calculate the absolute
+ *
+ *@n: - variable
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
