386c173775908a4b7903a33aef05df3499ab5598
Mounira2000 <tidjanimounira@gmail.com>
2023-02-25 17:48:08 -0800

    m

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		while (n >= 98)
+		while (n > 98)
 		{
 			printf("%d, ", n);
 			n--;
@@ -17,10 +17,15 @@ void print_to_98(int n)
 	}
 	else
 	{
-		while (n <= 98)
+		if (n == 98)
+		{
+			 printf("%d", n);
+		}
+		while (n < 98)
 		{
 			printf("%d, ", n);
 			n++;
 		}
+		
 	}
 }
