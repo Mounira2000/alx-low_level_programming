ffa5777c0155807370bd9612e911fa25a9bb0b3d
Mounira2000 <tidjanimounira@gmail.com>
2023-03-01 15:40:37 -0800

    f

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - function
+ * @dest: - variable
+ * @src: - variable
+ * Return: number
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int a = -1;
+	int i;
+
+	for (i = 0; dest[i] != '\0';)
+		i++;
+
+	do {
+		a++;
+		dest[i] = src[a];
+		i++;
+	} while (src[a] != '\0');
+	return (dest);
+}
+
