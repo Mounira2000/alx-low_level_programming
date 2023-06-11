055a6b8792fc2d2b970a3dd601467a4388ecd310
Mounira2000 <tidjanomounira@gmail.com>
2023-06-11 01:26:37 -0700

    exo 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * main - function
+ * @argc: var int
+ * @argv: var tableau char
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc >= 0)
+	{
+		_putchar(*argv[0]);
+		
+		_putchar ('\n');
+	}
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int exo_0(int argc, char* arg[]);
+
+#endif
