6410f6e21301e8a7e66b30b0daf41021f5e5d917
Mounira2000 <tidjanomounira@gmail.com>
2023-06-13 16:22:59 -0700

    exo 2

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/*
+ * _strdup - function that create a new variable
+ * @str: var
+ * Return: retur a pointer
+ */
+
+char *_strdup(char *str)
+{
+	int i, j;
+	int len = 0;
+	char *str2;
+
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	if (len != 0)
+	{	str2 = malloc(sizeof(char) * len);
+		if (str2 == NULL)
+			return (NULL);
+
+		for (j = 0; str[j] != '\0'; j++)
+		{
+			str2[j] = str[j];
+		}
+		return (str2);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
