49e1d84947ae58bfb5c39667328d40067daf94ab
Mounira2000 <tidjanomounira@gmail.com>
2023-06-13 18:02:04 -0700

    m

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,5 +10,30 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	
+	int i = 0, j = 0, k = 0;
+	char *conca;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s2[j] != '\0')
+		j++;
+	while (s1[i] != '\0')
+		i++;
+	conca = malloc(sizeof(char) * (i + j + 1));
+	if (conca == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		conca[k] = s1[i];
+		k++;
+	}
+	for (j = 0; s2[j] != '\0'; i++)
+	{
+		conca[k] = s2[j];
+		k++;
+	}
+	conca[k] = '\0';
+	return (conca);
 }
