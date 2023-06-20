89d3d1322fe490c2534dcd7e0ecf27868b1017d3
Mounira2000 <tidjanomounira@gmail.com>
2023-06-19 18:36:56 -0700

    s

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/*
+ * print_dog-betty docis not working i don't know why
+ * @d: var
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		;
+	else
+	{
+		if (d->name)
+			printf("Name: %s\n", d->name);
+		else
+			printf("nil\n");
+
+		printf("Age: %f\n", d->age);
+		if (d->owner == NULL)
+			printf("nil\n");
+		else
+			printf("Owner: %s\n", d->owner);
+
+	}
+}
